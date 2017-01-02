cab5423f7895036802ac7309f2e252c672474522
Galen Corey <galen.corey@gmail.com>
2017-01-02 11:13:16 -0500

    first draft of top words

--- utils.js
+++ utils.js
@@ -1,14 +1,37 @@
+const pluralize = require('pluralize');
+
 module.exports = {
   topWords: function(text){
     let seen = {};
-    let words = text.toLowerCase().replace(/\W/, '').split(/\s+/);
 
+    //let words = text.toLowerCase().replace(/\W/, '').split(/\s+/);
+    let words = text.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(/\s+/);
+
+    // go through each word, singularizing them and capitalize.
     words.forEach(word =>{
-      seen[word] = seen[word]? seen[word] + 1: 1;
-
+      let singularWord = pluralize.singular(word);
+      seen[singularWord] = seen[singularWord] ? seen[singularWord] + 1 : 1;
     })
 
+    let topWords = [];
+    let topCounts = [];
 
+    for (let word in seen){
+      if (topWords.length < 20) {
+        topWords.push(word)
+        topCounts.push(seen[word]);
+      }
+      else {
+        let min = Math.min(... topCounts);
+        if (seen[word] > min){
+          let index = topCounts.indexOf(min);
+          topWords[index] = word;
+          topCounts[index] = seen[word];
+        }
+      }
+    }
+
+    return [topCounts, topWords];
 
   }
 
