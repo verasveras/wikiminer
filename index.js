d00495267017e35ed38e60febf11155f0203c2e9
verasveras <lisaveras@gmail.com>
2017-01-02 10:32:30 -0500

    Getting back on it

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-const express = require('express');
-let app = express();
+// For making requests
+const request = require('request');
+
+// For markdown removal
+const strip = require('strip-markdown');
+const remark = require('remark');
+const processor = remark().use(strip);
+
+// Title of the page exactly as it appears!
+let articleTitle = 'Dogs'; /* TODO Figure out how well this works */
+
+request('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext&format=json&titles=dog', function (error, response, body) {
+  
+  if (!error && response.statusCode == 200) {
+
+  	/* TODO go through response header to see if you found one */
+
+  	// body = processor.process(body);
+	// let finalBody = String(body);
+
+	// let regExp = / {{.*}} /
+	// let matches = finalBody.match(regExp);
+	let json = JSON.parse(body);
+	jsonPages = json.query.pages;
 
 
+	for (var property in jsonPages) {
+		let text = jsonPages[property].extract;
+		text = processor.process(text);
+		text = String(text);
+		console.log(text);
+	}
 
+
+	// let regExp = /dogs/
+	// body = body.replace(regExp, '');
+	// console.log(body);
+
+  }
+
+})
---
+++ readme.txt
@@ -0,0 +1,14 @@
+const wikiminer = require('wikiminer');
+
+wikiminer('dogs')
+
+/*
+{
+	top_words: [],
+	worst_wors: [],
+	negative_words_%: #,
+
+	text: '' // stop words out, markdown
+	// other helpful things
+}
+*/
